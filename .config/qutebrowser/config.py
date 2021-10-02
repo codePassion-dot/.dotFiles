97f764a7906205339296aa86ddd617095acb1abf
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2021-10-01 23:51:16 -0500

    Added keyboard-oriented web browser and it's configuration file

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,15 @@
+import dracula.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+c.url.default_page = 'www.google.com'
+
+c.url.searchengines = {'DEFAULT': 'https://www.google.com/search?q={}','gh': 'https://github.com/search?q={}' }
+
