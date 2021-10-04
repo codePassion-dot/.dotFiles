09585000d211a6521f1ab8779938bc758c2c3185
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2021-10-04 17:46:41 -0500

    Added right-click binding to qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -13,3 +13,8 @@ c.url.default_page = 'www.google.com'
 
 c.url.searchengines = {'DEFAULT': 'https://www.google.com/search?q={}','gh': 'https://github.com/search?q={}' }
 
+
+#bindings
+config.bind('eo','hint all right-click')
+
+
