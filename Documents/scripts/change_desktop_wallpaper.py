98ebd22a2026b9243070b17c954df667f1922950
Juan Jacobo Jaramillo Aristizabal <jjjaramilloar@unal.edu.co>
2021-12-05 18:24:38 -0500

    Updated function to set only one wallpaper for one screen

--- Documents/scripts/change_desktop_wallpaper.py
+++ Documents/scripts/change_desktop_wallpaper.py
@@ -8,8 +8,7 @@ desktop_wallpapers_path = BACKGROUNDS + '/wallpapers/'
 images_folder = os.listdir(desktop_wallpapers_path)
 shell_program_command = 'feh --bg-scale'
 while True:
-    random_wallpaper_screen1 = randint(0,len(images_folder))
-    random_wallpaper_screen2 = randint(0,len(images_folder))
-    command_to_change_wallpaper = shell_program_command + ' ' + desktop_wallpapers_path + images_folder[random_wallpaper_screen1] + ' ' + desktop_wallpapers_path + images_folder[random_wallpaper_screen2]
+    random_wallpaper_screen = randint(0,len(images_folder))
+    command_to_change_wallpaper = shell_program_command + ' ' + desktop_wallpapers_path + images_folder[random_wallpaper_screen]
     os.system(command_to_change_wallpaper)
     time.sleep(300)
