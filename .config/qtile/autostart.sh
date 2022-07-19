b731c6d91f35afbb06bc37d64dc3fd5d7bd48b9c
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2022-07-19 14:05:56 -0500

    added picom to the startup (again) but with different opacities

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,7 @@
-#! /bin/bash 
+#! /bin/bash
 xbindkeys &
 python3 "$SCRIPTS/change_desktop_wallpaper.py" &
-#picom &
+picom &
 copyq &
 flameshot &
 #noisetorch -s alsa_input.usb-Corsair_CORSAIR_VOID_ELITE_Wireless_Gaming_Dongle-00.mono-fallback -i &
