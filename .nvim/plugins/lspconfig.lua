28c6a847cd60ddbb9c0f0bf8ead318293fe657a4
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2022-08-08 21:34:30 -0500

    add setting to tsserver to ingnore typechecking in js files

--- .nvim/plugins/lspconfig.lua
+++ .nvim/plugins/lspconfig.lua
@@ -1,20 +1,26 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
-local servers = { "html", "cssls", "bashls", "emmet_ls", "tsserver", "tailwindcss" }
+local servers = { "html", "volar", "cssls", "bashls", "emmet_ls", "tsserver", "tailwindcss" }
 
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = function(client, bufnr)
-			on_attach(client, bufnr)
-			if lsp == "tsserver" then
-				local ts_utils = require("nvim-lsp-ts-utils")
-				ts_utils.setup({
-					filter_out_diagnostics_by_severity = { "hint" },
-				})
-				ts_utils.setup_client(client)
-			end
-		end,
-		capabilities = capabilities,
-	})
+	if lsp == "tsserver" then
+		lspconfig.tsserver.setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			settings = {
+				diagnostics = {
+					ignoredCodes = {
+						-- https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+						7016, -- ts7016: The file is not a module.
+					},
+				},
+			},
+		})
+	else
+		lspconfig[lsp].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+		})
+	end
 end
