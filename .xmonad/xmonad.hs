5e36463dcf4a7a92985eaf498301d01554234aac
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2022-10-17 13:23:57 -0500

    added default layout for xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,19 @@
 import           XMonad
 
+import           XMonad.Layout.Magnifier
+import           XMonad.Layout.ThreeColumns
 import           XMonad.Util.EZConfig
 import           XMonad.Util.Ungrab
 
+baseLayout = tiled ||| Mirror tiled ||| Full
+ where
+  threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
+  tiled    = Tall nmaster delta ratio
+  nmaster  = 1 -- Default number of windows in the master pane
+  ratio    = 1 / 2 -- Default proportion of screen occupied by master pane
+  delta    = 3 / 100 -- Percent of screen to increment by when resizing panes
+
 main :: IO ()
 main =
-  xmonad $ def { modMask = mod4Mask }  -- Rebind Mod to the Super key
+  xmonad $ def { modMask = mod4Mask, -- Rebind Mod to the Super key
+                                     layoutHook = baseLayout } -- Use custom layouts 
