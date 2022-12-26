75ff6b2fd6f7ad2da4fd51a0bafce54a64e28a2c
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2022-12-25 20:57:14 -0500

    add linter and lsp for markdown files

--- .nvim/plugins/lspconfig.lua
+++ .nvim/plugins/lspconfig.lua
@@ -1,7 +1,19 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
-local servers = { "html", "volar", "cssls", "bashls", "emmet_ls", "tsserver", "tailwindcss", "vuels", "hls", "gopls" }
+local servers = {
+	"html",
+	"volar",
+	"cssls",
+	"bashls",
+	"emmet_ls",
+	"tsserver",
+	"tailwindcss",
+	"vuels",
+	"hls",
+	"gopls",
+	"grammarly",
+}
 
 for _, lsp in ipairs(servers) do
 	if lsp == "tsserver" then
