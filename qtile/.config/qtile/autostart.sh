bbb65f0aaf6ef842afec8978174cfc2bce9cd83e
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2023-05-26 22:20:19 -0500

    update rclone paths and add to startup applications

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -5,12 +5,12 @@ picom &
 copyq &
 flameshot &
 #rclone mount points to google drive
-rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full gd:"PROYECTO IDEA" "$HOME/Documents/IDEA-REMOTE"
-rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full gd:"PERSONAL-WORK" "$HOME/Documents/PERSONAL-WORK-REMOTE"
-rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full gd:"DATOS CASA (PINAR)" "$HOME/Documents/HOME-DATA-REMOTE"
-rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full gd:"UNIVERSIDAD NACIONAL" "$HOME/Documents/UNIVERSITY-REMOTE"
+rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full drive-volatile:"PROYECTO IDEA" "$HOME/Documents/IDEA-REMOTE"
+rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full drive-volatile:"PERSONAL-WORK" "$HOME/Documents/PERSONAL-WORK-REMOTE"
+rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full drive-volatile:"DATOS CASA (PINAR)" "$HOME/Documents/HOME-DATA-REMOTE"
+rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full drive-volatile:"UNIVERSIDAD NACIONAL" "$HOME/Documents/UNIVERSITY-REMOTE"
 rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full gdd: "$HOME/Documents/PERSONAL-DATA-REMOTE"
-rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full gd:"DEV BOOKS" "$HOME/Documents/BOOKS"
+rclone mount --daemon --dir-cache-time 30m --vfs-cache-mode full drive-volatile:"DEV BOOKS" "$HOME/Documents/BOOKS"
 xrdb "$HOME/.Xresources"
 noisetorch -s alsa_input.usb-Corsair_CORSAIR_VOID_ELITE_Wireless_Gaming_Dongle-00.mono-fallback -i &
 #redshift &
