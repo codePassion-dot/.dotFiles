6f8effd2a0a4c10372588f07247e69ec87402ef9
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2023-06-22 08:22:51 -0500

    finally nvim typescript debugger is working

--- nvim/.config/nvim/lua/plugins/javascript.lua
+++ nvim/.config/nvim/lua/plugins/javascript.lua
@@ -1,76 +1,53 @@
-return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
+local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+
+for _, language in ipairs(js_based_languages) do
+  require("dap").configurations[language] = {
     {
-      "mxsdev/nvim-dap-vscode-js",
-      module = { "dap-vscode-js" },
-      opts = {
-        node_path = "node",
-        debugger_path = os.getenv("HOME") .. "/.local/share/nvim/lazy/vscode-js-debug",
-        -- debugger_cmd = {"js-debug-adapter"}
-        adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-      },
-      config = function()
-        local dap = require("dap")
-        for _, language in ipairs({ "typescript", "javascript" }) do
-          dap.configurations[language] = {
-            {
-              type = "pwa-node",
-              request = "launch",
-              name = "Launch file",
-              program = "${file}",
-              cwd = "${workspaceFolder}",
-            },
-            {
-              type = "pwa-node",
-              request = "attach",
-              name = "Attach",
-              processId = require("dap.utils").pick_process,
-              cwd = "${workspaceFolder}",
-            },
-            {
-              type = "pwa-node",
-              request = "launch",
-              name = "Debug Jest Tests",
-              -- trace = true, -- include debugger info
-              runtimeExecutable = "node",
-              runtimeArgs = {
-                "./node_modules/jest/bin/jest.js",
-                "--runInBand",
-              },
-              rootPath = "${workspaceFolder}",
-              cwd = "${workspaceFolder}",
-              console = "integratedTerminal",
-              internalConsoleOptions = "neverOpen",
-            },
-          }
-        end
-        for _, language in ipairs({ "typescriptreact", "javascriptreact" }) do
-          require("dap").configurations[language] = {
-            {
-              type = "pwa-chrome",
-              name = "Attach - Remote Debugging",
-              request = "attach",
-              program = "${file}",
-              cwd = vim.fn.getcwd(),
-              sourceMaps = true,
-              protocol = "inspector",
-              port = 9222,
-              webRoot = "${workspaceFolder}",
-            },
-            {
-              type = "pwa-chrome",
-              name = "Launch Chrome",
-              request = "launch",
-              url = "http://localhost:3000",
-            },
-          }
-        end
-      end,
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
     },
     {
-      "microsoft/vscode-js-debug",
-      build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+      type = "pwa-node",
+      request = "attach",
+      name = "Attach",
+      processId = require("dap.utils").pick_process,
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "pwa-chrome",
+      request = "launch",
+      name = 'Start Chrome with "localhost"',
+      url = "http://localhost:3000",
+      webRoot = "${workspaceFolder}",
+      userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+    },
+  }
+end
+
+return {
+  { "mfussenegger/nvim-dap" },
+  {
+    "mxsdev/nvim-dap-vscode-js",
+    opts = {
+      -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+      debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+      -- debugger_cmd = { "extension" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+      adapters = {
+        "chrome",
+        "pwa-node",
+        "pwa-chrome",
+        "pwa-msedge",
+        "node-terminal",
+        "pwa-extensionHost",
+        "node",
+        "chrome",
+      },
+      -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+      -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+      -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
     },
   },
 }
