946d2e5fbb0dbd3800bc2312fea35d9f87c04227
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2023-07-03 11:42:09 -0500

    set_wallpaper built-in awesomewm is bullshit just use feh

--- awesome/.config/awesome/mytheme.lua
+++ awesome/.config/awesome/mytheme.lua
@@ -8,7 +8,6 @@ local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
-local wallpapers_path = "~/Pictures/"
 
 local theme = {}
 
@@ -94,8 +93,6 @@ theme.titlebar_maximized_button_normal_inactive = themes_path .. "default/titleb
 theme.titlebar_maximized_button_focus_inactive = themes_path .. "default/titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_active = themes_path .. "default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active = themes_path .. "default/titlebar/maximized_focus_active.png"
-
-theme.wallpaper = wallpapers_path .. "relax-waifu.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path .. "default/layouts/fairhw.png"
