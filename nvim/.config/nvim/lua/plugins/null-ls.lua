70d7d0a995aa2daffcad58a41a94c671d8d53c64
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2023-08-10 18:38:09 -0500

    add black formatter for python code

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -10,6 +10,7 @@ return {
         nls.builtins.formatting.prettier,
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.phpcsfixer,
+        nls.builtins.formatting.black,
         nls.builtins.diagnostics.stylelint.with({
           filetypes = { "scss", "less", "css", "sass", "typescriptreact", "javascriptreact" },
         }),
