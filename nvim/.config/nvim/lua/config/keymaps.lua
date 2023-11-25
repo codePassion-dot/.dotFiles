f811b7495b856ced75ced938b89d1b817a0633d9
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2023-11-25 10:26:31 -0500

    remove some plugins

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,3 @@
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set("n", "<leader>fm", function()
-  require("dropbar.api").pick()
-end, { desc = "set interactive breadcrumb pick" })
