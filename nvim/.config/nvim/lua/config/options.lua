60f31ecd50c2c62003f068ae398a9baa0910d4c3
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2024-04-13 10:30:02 -0500

    do not share clipboard between nvim and system

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -9,3 +9,4 @@ opt.foldcolumn = "1" -- '0' is not bad
 opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 opt.foldlevelstart = 99
 opt.foldenable = true
+opt.clipboard = "unnamed"
