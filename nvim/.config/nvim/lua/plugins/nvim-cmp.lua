683d4be7befdeb5f91f3a1b073e0c45ed3aef8b2
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2024-05-15 09:58:51 -0500

    use fixed commit version for crates nvim while lazyvim deploy another release

---
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "Saecki/crates.nvim",
+        commit = "786d12a70c9b91fa2d0d102bb07df02be0db31a1",
+      },
+    },
+  },
+}
