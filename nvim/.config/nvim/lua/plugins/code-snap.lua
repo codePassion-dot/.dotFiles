ef9fa06e0691ab51a69c5c703d8c15aeb7424973
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2025-11-09 10:50:35 -0500

    fix codesnap keymap

--- nvim/.config/nvim/lua/plugins/code-snap.lua
+++ nvim/.config/nvim/lua/plugins/code-snap.lua
@@ -2,6 +2,9 @@ return {
   {
     "mistricky/codesnap.nvim",
     build = "make build_generator",
+    keys = {
+      { "<leader>cs", "<Esc><cmd>CodeSnap<cr>", mode = "x", desc = "code screenshot" },
+    },
     opts = {
       save_path = "~/Pictures",
       watermark = "",
